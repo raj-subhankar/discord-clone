bd5b0d0b5f5edef7ed2007541e3f67372238790d
raj-subhankar <subhankar.rj@gmail.com>
2020-12-26 14:41:19 +0530

    Finish Sidebar UI

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,14 @@ import React from "react"
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
 import './Sidebar.css'
 import AddIcon from "@material-ui/icons/Add"
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt"
 import SidebarChannel from "./SidebarChannel"
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
+import CallIcon from "@material-ui/icons/Call"
+import { Avatar } from "@material-ui/core"
+import MicIcon from "@material-ui/icons/Mic"
+import HeadsetIcon from "@material-ui/icons/Headset"
+import SettingsIcon from "@material-ui/icons/Settings"
 
 function Sidebar() {
     return(
@@ -21,10 +28,42 @@ function Sidebar() {
 
                     <AddIcon className="sidebar__addChannel"/>
                 </div>
+
+                <div className="sidebar__channelsList">
+                    <SidebarChannel />
+                    <SidebarChannel />
+                    <SidebarChannel />
+                </div>
             </div>
 
-            <div className="sidebar__channelsList">
-                <SidebarChannel />
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon 
+                    className="sidebar__voiceIcon"
+                    fontSize="large"
+                />
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar />
+                <div className="sidebar__profileInfo">
+                    <h3>Subhankar</h3>
+                    <p>#thisIsMyId</p>
+                </div>
+
+                <div className="sidebar__profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                </div>
             </div>
         </div>
     )
