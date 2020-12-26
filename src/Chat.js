e050e31c65f8b2302f1bf9615005f6b85dc0c209
raj-subhankar <subhankar.rj@gmail.com>
2020-12-26 16:39:57 +0530

    Implement Chat input UI

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,33 @@
 import React from "react"
 import "./Chat.css"
 import ChatHeader from "./ChatHeader"
+import AddCircleIcon from "@material-ui/icons/AddCircle"
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions"
+import GifIcon from "@material-ui/icons/Gif"
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard"
 
 function Chat() {
     return(
         <div className="chat">
-            <ChatHeader />
+            <ChatHeader /> 
+
+            <div className="chat__messages">
+            
+            </div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+                <form>
+                    <input placeholder={`Message #Test Channel`} />
+                    <button className="chat__inputButton" type="submit">Send message</button>
+                </form>  
+
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large" />
+                    <GifIcon fontSize="large" />
+                    <EmojiEmotionsIcon fontSize="large" />
+                </div>
+            </div> 
         </div>
     )
 }
