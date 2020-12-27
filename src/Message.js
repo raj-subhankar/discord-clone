ce7fc8f66dda7a62e4888a96e4622a5c374107b9
raj-subhankar <subhankar.rj@gmail.com>
2020-12-27 17:42:40 +0530

    Send and show messages

--- src/Message.js
+++ src/Message.js
@@ -2,16 +2,18 @@ import { Avatar } from "@material-ui/core"
 import React from "react"
 import "./Message.css"
 
-function Message() {
+function Message({ timestamp, user, message }) {
     return(
         <div className="message">
-            <Avatar />  
+            <Avatar src={user.photo} />  
             <div className="message__info">
-                <h4>subh
-                    <span className="message__timestamp">this is a timestamp</span>
+                <h4>{user.displayName}
+                    <span className="message__timestamp">
+                        {new Date(timestamp?.toDate()).toUTCStringk}
+                    </span>
                 </h4>
 
-                <p>This is a message</p>
+                <p>{message}</p>
             </div>
 
         </div>
